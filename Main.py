f2d0a7d7a38b74333c82bafef5c9af12e9c736b6
ByyChase <zwtw@iup.edu>
2021-02-13 00:21:23 -0500

    Ability to read and write to the database added

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-import re, os, csv
+import re, os, csv, sqlite3
 import ipwhois
 
 
@@ -36,31 +36,18 @@ def load_DB(db_file):
     #Checks to see if the database file exists. If it does not the database will be created 
     if os.path.isfile('ip_checker.db'):
 
-        try:
+       
 
-            #create connection with the database 
-            db = sqlite3.connect(db_file)
-            logging.info("Database successfully loaded")
-
-        except Exception as e:
-
-            logging.exception("Unable to connect to database, attempting to create database")
-            pass
+        #create connection with the database 
+        db = sqlite3.connect(db_file)
     
     else:
 
-        try:
+        #Creates the database files
+        db = sqlite3.connect(db_file)
+        #Calls the create_db method to create the database tables 
+        create_DB(db.cursor())
 
-            #Creates the database files
-            db = sqlite3.connect(db_file)
-            #Calls the create_db method to create the database tables 
-            create_DB(db.cursor())
-
-        except Exception as e:
-
-            logging.exception("Unable to create database, closing program")
-            print("Critial error creating database, see logs for more information.\n\nExiting Program")
-            exit()
 
     return db.cursor()
 
@@ -98,13 +85,12 @@ def create_DB(c):
 
     try:
         c.execute("""CREATE TABLE IPADDRESS (
-                        Date text,
-                        Amount real,
-                        UnBudgeted real,
-                        Description text,
-                        IncomeStatement_ID integer PRIMARY KEY,
-                        User_ID int,
-                        foreign key(User_ID) references User(User_ID)
+                        IP_ADDRESS text PRIMARY KEY,
+                        DESCRIPTION text,
+                        IP_RANGE test,
+                        DATE text,
+                        TIMES_FOUND text,
+                        BLOCKED text
                         )""")
 
 
@@ -113,108 +99,79 @@ def create_DB(c):
     
     return
 
+def commit_ip_address(ip_address, description, ip_range, date, times_found, blocked):
+
+    statement = "INSERT INTO IPADDRESS (IP_ADDRESS, DESCRIPTION, IP_RANGE, DATE, TIMES_FOUND, BLOCKED) VALUES (?, ?, ?, ?, ?, ?)"
+    cursor().execute(statement, (ip_address, description, ip_range, date, times_found, blocked))
+    commit()
 
 
+def fetch(ip_address):
+
+    statement = "FROM IPADDRESS SELECT * WHERE IP_ADDRESS = ?"
+    ip_address_data = cursor().execute(statement, (ip_address, description, ip_range, date, times_found, blocked)).fetchone()
+
+    if ip_address_data: 
+        return ip_address_data
+    
+    else:
+        return "0"
 
 
-cont = True
-Program_Folder_Path = str(os.path.expanduser('~/Documents') + '/PythonIPChecker')
-IP_Address_List = []
-IP_Address_Found = 0
+def update(number, which_number):
+
+    if which_number == "1":
+        pass
+
+    elif which_number == "2":
+        pass
+
+
 regex = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
             25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
             25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
             25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)$'''
 
-IP_Address_Temp = '0'
-
-
 
 #Functions
-def main(IP_Address_Temp):
+def main():
 
-    IP_Address_Found = 0
-    CSV_Header_Names = ['IP Address', 'Owner', 'Date of Registration', 'IP Range Owned', 'Times Found in Emails', 'Blocked']
 
-    if cont:
-        IP_Address = str(input('Please input an IP address: '))
+   
+    IP_Address = str(input('Please input an IP address: '))
 
-        if IP_Address.lower == 'exit':
-            exit()
+    if IP_Address.lower == 'exit':
+        exit()
 
-        if check(IP_Address):
-            pass
+    if check(IP_Address):
+        pass
 
-        else:
-            False
+    else:
+        False
 
     
     print('\n')
 
-    if IP_Address_Temp == IP_Address:
-        Check_Same_IP = input("This is the same address you put in right before this. Did you mean to do that? \n\nInput (Yes/No): ")
-        while Check_Same_IP.lower() != 'yes' and Check_Same_IP.lower() != 'no':
-            Check_Same_IP = input("\n\tPlease only input Yes or no: ")
 
-        if Check_Same_IP.lower() == 'yes':
-            print("\n")
+    IP_data = ipwhois.IPWhois(IP_Address).lookup_rdap()
+    IPList = [IP_data.get('query'), IP_data.get('asn_description'), IP_data.get('asn_date'), IP_data.get('asn_cidr'), '1', '0']
 
-        else:
-            print("\nWe will restart the script then")
-            main(IP_Address_Temp)
+    if fetch(IP_data.get('query')):
+        print("This address has already been added to the database")
 
-    IP_Address_Temp = IP_Address
-
+    else:
+        commit_ip_address(IP_data.get('query'), IP_data.get('asn_description'), IP_data.get('asn_cidr'), IP_data.get('asn_date'), '1', '0')
+        print("\n\n------------------------------------------\n|The following entry has been added to the database|\n------------------------------------------: ")
+        print("IP Address: " + IPList[0] + + "\nIP Range: " + IPList[3] + "\nDescription: " + IPList[1] + "\nDate: " + IPList[2]) 
+        print("\n\nThe IP Address has been added t o the database")
 
 
-    for x in IP_Address_List:
-        if x[0] == IP_Address:
-            x[4] = int(x[4]) + 1
+    
 
-            if x[4] == 1:
-
-                print('\n\nThe CSV file has been updated with this found IP address.')
-                Block = input("Are you going to block this IP address? \n\nInput (Yes/No): ")
-
-                while Block != 'yes' and Block != 'YES' and Block != 'Yes' and Block != 'no' and Block != 'NO' and Block != 'No':
-                    Block = input("\n\tPlease only input Yes or no: ")
-
-                if Block == 'Yes' or Block == 'yes' or Block == 'YES':
-                    x[5] == '1'
-                IP_Address_Found = 1
-
-                IP_Address_Temp = IP_Address
-
-            else:
-                print('\nThis address has been found ' + str(x[4]) + ' times already')
-                Block = input("Are you going to block this IP address? \n\nInput (Yes/No): ")
-
-                while Block != 'yes' and Block != 'YES' and Block != 'Yes' and Block != 'no' and Block != 'NO' and Block != 'No':
-                    Block = input("\n\tPlease only input Yes or no: ")
-
-                if Block == 'Yes' or Block == 'yes' or Block == 'YES':
-                    x[5] == '1'
-                IP_Address_Found = 1
-
-                IP_Address_Temp = IP_Address
+    
 
 
-    if IP_Address_Found == 0:
-        IP_data = ipwhois.IPWhois(IP_Address).lookup_rdap()
-        IPList = [IP_data.get('query'), IP_data.get('asn_description'), IP_data.get('asn_date'), IP_data.get('asn_cidr'), '1', '0']
-        IP_Address_List.append(IPList)
-        print("\nThe IP Address has been added to the CSV file")
-
-
-    """
-    What is in the CSV
-    0 IP Address
-    1 ASN Description 
-    2 ASN Date
-    3 ASN IP Range
-    4 Times Found 
-    5 Blocked
-
+ 
     Repeat = input("Would you like to add another IP Address? \n\nInput (Yes/No):")
     while Repeat != 'yes' and Repeat != 'YES' and Repeat != 'Yes' and Repeat != 'no' and Repeat != 'NO' and Repeat != 'No':
         Repeat = input("\n\tPlease only input Yes or no: ")
@@ -233,6 +190,13 @@ print("|  | |   ___/     |  |     |   __   | |   __|  |  |     |    <   |   __| 
 print("|  | |  |         |  `----.|  |  |  | |  |____ |  `----.|  .  \  |  |____ |  |\  \----. ")
 print("|__| |__|          \ _____||__|  |__| |_______| \______||__|\__\ |_______|| _| `._____| ")
 print("Please type 'Exit' at anytime to quit the program\n")
+
+try:
+    load_DB(os.getcwd() + '/ip_checker.db')
+
+except Exception as e:
+    print("\n\nCould not load Database")
+    print("\n\n" + str(e))
 
 
 main()
